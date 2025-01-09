79fa68e068efa1cf4ae077422406b33ecceb8e39
NotRin7 <arianhashani07@gmail.com>
2025-01-09 17:10:35 +0100

    -

--- notrin-test-litecoin/exports.sh
+++ notrin-test-litecoin/exports.sh
@@ -9,5 +9,5 @@ export LITECOIN_RPC_PORT_1="9332"
 export LITECOIN_RPC_PORT_2="9333"
 export LITECOIN_DASHBOARD_PORT="10102"
 
-export APP_LITECOIN_NODE_IP_2="host.docker.internal"
-export APP_LITECOIN_FLESK_IP_2="host.docker.internal"
+export APP_LITECOIN_NODE_IP_2="10.21.0.1"
+export APP_LITECOIN_FLESK_IP_2="10.21.0.1"
