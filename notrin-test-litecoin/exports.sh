a42fcb09234908c9fbfd2db6398a9fdd3760755f
NotRin7 <arianhashani07@gmail.com>
2025-01-09 16:24:01 +0100

    -

---
+++ notrin-test-litecoin/exports.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export LITECOIN_RPC_IP="litecoin_1"
+export LITECOIN_RPC_PORT="9332"
+export LITECOIN_API_IP="192.168.1.33"
+
+export LITECOIN_RPC_USER ="testuser"
+export LITECOIN_RPC_PASSWORD ="testpass"
+
+export LITECOIN_RPC_PORT_1="9332"
+export LITECOIN_RPC_PORT_2="9333"
+export LITECOIN_DASHBOARD_PORT="10102"
+
+export APP_LITECOIN_NODE_IP="10.21.33.33"
+export APP_LITECOIN_FLESK_IP="10.21.33.34"
